e337d2e0277b871815cdbb7c1ca5827d4beb9d69
metastableB <donkdennis@gmail.com>
2017-03-31 15:28:29 +0530

    Added a sample time output to automatedRuns script

--- scripts/automatedRuns.sh
+++ scripts/automatedRuns.sh
@@ -5,6 +5,14 @@
 #
 # Automatically run simulation without text graphics
 # for a specified number of times
+#
+# Here is a sample Time output for referrence
+# 	time ./automatedRuns.sh > '00_100_ReflexAgent_RandomFoodAgent.csv'
+# 	real	1m23.778s
+# 	user	1m29.413s
+# 	sys	0m54.193s
+# RandomFoodAgnet and ReflexAgent were used for 100 trials
+
 
 # TODO: Move to command line arguments
 numRuns=10
@@ -17,5 +25,5 @@ export PYTHONPATH=${PYTHONPATH}:${dlsnakePATH}
 
 # Run for numRun tries
 for i in $(seq 1 ${numRuns}); do
-	python -m dlsnake.snakeGame --agent ${snakeAgent} --no-graphics --silent --csv
+	python -m dlsnake.snakeGame --agent ${snakeAgent} --silent --csv
 done
