77794c39942ced2fc9ec0ba2a5286309070f39a7
metastableB <donkdennis@gmail.com>
2017-04-05 21:57:24 +0530

    Added abstract update() to Agent class

--- dlsnake/agents/agent.py
+++ dlsnake/agents/agent.py
@@ -19,3 +19,15 @@ class Agent:
         return a action among the valid actions for the game
         '''
         pass
+
+    def update(self, state, action, reward, nextState):
+        '''
+        Used for Q-learning and approximate q-learning
+        based agents, wherein after each action, we have
+        to update the internal weights as well
+        Call this method after each move of game state.
+        THERE IS NO WAY YET TO VALIDATE IF THIS METHOD
+        IS BEING CALLED PROPERLY AND THE ONUS TO DO THAT
+        IS ON THE USER.
+        '''
+        pass
